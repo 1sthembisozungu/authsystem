6e550aa9242e34372c43c88dfc951c8994fc1d7c
1sthembisozungu <1sthembisozungu@gmail.com>
2026-08-17 12:24:53 +0200

    created login.html and login.js with others file

--- app.py
+++ app.py
@@ -6,9 +6,13 @@ app = Flask(__name__)
 def register():
     return render_template("register.html")
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    return "Login page coming soon"
+    return render_template("login.html")
+
+@app.route("/forgot-password")
+def forgot_password():
+    return "Forgot password page coming soon"
 
 
 if __name__ == "__main__":
